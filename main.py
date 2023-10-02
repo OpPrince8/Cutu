dd452cefe4614a6c3761fc2d9ee5df224fa5e6c4
OpPrince8 <87240259+OpPrince8@users.noreply.github.com>
2023-10-02 12:00:09 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+import os
+from webserver import keep_alive
+
+client = commands.Bot(command_prefix = '??', intents = discord.Intents.all())
+
+
+@client.event
+async def on_ready():
+    await client.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.listening, name="'Op Prince' ??hello"))
+    print("Bot is Ready and Online")
+
+@client.command()
+async def hello(ctx):
+  await ctx.send("Hello! I am Cutu. Made by Op prince I am under development right now will soon be done.")
+
+my_secret = os.environ['TOKEN']
+
+keep_alive() 
+client.run(os.getenv("TOKEN"))
